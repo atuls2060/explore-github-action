bf0922a6771f4983cc9b6754fe33f4c4d1b72916
atulsinghd11 <138652754+atulsinghd11@users.noreply.github.com>
2024-12-05 15:09:55 +0530

    deploy to netlify

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Simple React App</p>
+        <p>New campaign</p>
       </header>
       
     </div>
